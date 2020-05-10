95207b316d5d9244791b9fee9896131f9359fbee
Amir Shahein <shaheinamir@gmail.com>
2020-05-10 14:29:28 +0200

    removed generate_overlapping_clusters and just consolidated it into generate_clusters

--- pwm_scan/main.py
+++ pwm_scan/main.py
@@ -411,50 +411,12 @@ class PWMScan(object):
                 affinitiesTemp = []
                 startPosTemp = []
                 strandTemp = []
-                
-        self.unpClusterList = unpClusterList
         
-    def generate_overlapping_clusters(self):
-        
-        """
-        maxGap: fixed here. 
-        """
-        
-        ovlpClusterList = []
-        
-        sitesTemp = [] #Declare temporary variables used to build clusters and transfer to unpClusterList 
-        affinitiesTemp = []
-        startPosTemp = []
-        strandTemp = []
-        
-        for i in tqdm(range(len(self.hits.index)-1)): #For each index in self.hits
-            #gapDist = self.hits.loc[i+1].Start - self.hits.loc[i].End -1   
-         
-            #if the distance to the next site is less than the maximum gap distance input, but greater than the minimum gap distance (biggest overlap allowed)
-            #if #((gapDist <= maxGap) and (gapDist >= minGap)) -- note, it will make some sense to incorporate this at some point.
-            if ((self.hits.loc[i+1].Start - self.hits.loc[i].End -1) <= -1):
-                sitesTemp.append(self.hits.loc[i].Sequence)
-                affinitiesTemp.append(self.hits.loc[i].Score)
-                startPosTemp.append(self.hits.loc[i].Start)
-                strandTemp.append(self.hits.loc[i].Strand)
-            
-            elif (sitesTemp) : 
-                #if sitesTemp is not empty (non- vs. empty list has boolean true false), and therefore we're on the last member of a cluster
-                sitesTemp.append(self.hits.loc[i].Sequence) #append the last member of the cluster to the temporary variables
-                affinitiesTemp.append(self.hits.loc[i].Score)
-                startPosTemp.append(self.hits.loc[i].Start)
-                strandTemp.append(self.hits.loc[i].Strand)     
-                
-                #Transform temporary variables into a Clusters object and store it as an element of the list ovlpClusterClist
-                ovlpClusterList.append(Clusters(sitesTemp, affinitiesTemp, startPosTemp, strandTemp))
-                
-                sitesTemp = [] #Clear temporary variables
-                affinitiesTemp = []
-                startPosTemp = []
-                strandTemp = []
-                
-        self.ovlpClusterList = ovlpClusterList        
-                 
+        if (maxGap <= -1):
+            self.ovlpClusterList = unpClusterList
+        else:
+            self.unpClusterList = unpClusterList
+    
 @dataclass    
 class Clusters:
     bindingSiteSeqs: list
